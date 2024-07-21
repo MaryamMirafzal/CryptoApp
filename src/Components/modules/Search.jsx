d214897f5fd5f58ad250649abbe9ec831603a372
Maryam Mirafzal <maryammirafzal9@gmail.com>
2024-07-21 17:28:06 +0330

    change currency about coins

---
+++ src/Components/modules/Search.jsx
@@ -0,0 +1,15 @@
+
+function Search({currency , setCurrency}) {
+  return (
+    <div>
+        <input type="text" placeholder="Search" />
+        <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="jpy">JPY</option>
+        </select>
+    </div>
+  )
+}
+
+export default Search
--- src/Components/templates/HomPage.jsx
+++ src/Components/templates/HomPage.jsx
@@ -3,25 +3,28 @@ import { useEffect, useState } from "react"
 import TableCoin from "../modules/TableCoin"
 import { getCoinList } from "../../Services/cryptoApi"
 import Pagination from "../modules/Pagination"
+import Search from "../modules/Search"
 
 function HomPage() {
   const [ coins , setCoins ] = useState([])
   const [ page , setPage ] = useState(1)
   const [ isLoading , setIsLoading ] = useState(true)
+  const [currency , setCurrency ] = useState("usd")
   useEffect(()=>{
     const getData = async ()=>{
-      const res = await fetch (getCoinList(page))
+      const res = await fetch (getCoinList(page , currency))
       const json = await res.json();
       setCoins(json)
       setIsLoading(false)
     }
     getData();
-    },[page])
+    },[page , currency])
 
 
 
   return (
     <div>
+      <Search currency={currency} setCurrency={setCurrency}/>
       <TableCoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
